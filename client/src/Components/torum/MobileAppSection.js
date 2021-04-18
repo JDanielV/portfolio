a3fcfec1fc3f3dc9e21d7c03a79382fdcede1513
Daniel Villaverde <52183446+JDanielV@users.noreply.github.com>
2021-04-18 15:51:23 -0700

    Added coin icons, classes & starting positions

--- client/src/Components/torum/MobileAppSection.js
+++ client/src/Components/torum/MobileAppSection.js
@@ -1,8 +1,54 @@
 import React from 'react';
 import "../../styles/main.css";
 import MobilePhoneImg from "../../assets/torum-assets/images/social-mobile.webp";
+import NeoIcon from "../../assets/torum-assets/images/neo-icon.webp"
+import UsdtIcon from "../../assets/torum-assets/images/usdt-icon.webp"
+import TorumCoinIcon from "../../assets/torum-assets/images/torum-coin-icon.webp"
+import MoneroIcon from "../../assets/torum-assets/images/monero-icon.webp"
+import ShtCoinIcon from "../../assets/torum-assets/images/shtcoin-icon.webp"
+import BtcIcon from "../../assets/torum-assets/images/btc-icon.webp"
+import EthIcon from "../../assets/torum-assets/images/eth-icon.webp"
 
 const MobileAppSection = () => {
+
+    const coinIconsArray = [
+        {
+            icon: NeoIcon,
+            iconClass: "torum-mobile-app__right-img-floating-icon-neo",
+            iconAlt: "neo coin"
+        },
+        {
+            icon: UsdtIcon,
+            iconClass: "torum-mobile-app__right-img-floating-icon-usdt",
+            iconAlt: "USDT coin"
+        },
+        {
+            icon: TorumCoinIcon,
+            iconClass: "torum-mobile-app__right-img-floating-icon-torum",
+            iconAlt: "torum coin"
+        },
+        {
+            icon: MoneroIcon,
+            iconClass: "torum-mobile-app__right-img-floating-icon-monero",
+            iconAlt: "monero coin"
+        },
+        {
+            icon: ShtCoinIcon,
+            iconClass: "torum-mobile-app__right-img-floating-icon-shtcoin",
+            iconAlt: "poop coin"
+        },
+        {
+            icon: BtcIcon,
+            iconClass: "torum-mobile-app__right-img-floating-icon-btc",
+            iconAlt: "BTC coin"
+        },
+        {
+            icon: EthIcon,
+            iconClass: "torum-mobile-app__right-img-floating-icon-eth",
+            iconAlt: "ETH coin"
+        },
+    ]
+
     return (
         <div className="torum-mobile-app">
             <div className="torum-mobile-app__left-wrapper">
@@ -11,7 +57,12 @@ const MobileAppSection = () => {
                 <button className="torum-mobile-app__button">coming soon</button>
             </div>
             <div className="torum-mobile-app__right-wrapper">
-                <img className="torum-mobile-app__right-img" src={MobilePhoneImg} alt="mobile app"/>
+                <img className="torum-mobile-app__right-img" src={MobilePhoneImg} alt="mobile app" />
+                {coinIconsArray.map((coin) => {
+                    return (
+                        <img className={coin.iconClass} src={coin.icon} alt={coin.iconAlt} />
+                    )
+                })}
             </div>
         </div>
     )
