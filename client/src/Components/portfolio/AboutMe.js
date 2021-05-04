f92f4d140681581e96b7e90e54cc62d1c19026ea
Daniel Villaverde <52183446+JDanielV@users.noreply.github.com>
2021-05-04 16:41:23 -0700

    finished styling pass to ABOUT section

--- client/src/Components/portfolio/AboutMe.js
+++ client/src/Components/portfolio/AboutMe.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import "../../styles/main.css";
+import AboutMeImg from "../../assets/portfolio-assets/about-me-pic.jpg";
 
 const AboutMe = () => {
     return (
         <section className="portfolio-about-me">
             <div className="portfolio-about-me__img-wrapper">
-                <img className="portfolio-about-me__img" src="" alt="" />
+                <img className="portfolio-about-me__img" src={AboutMeImg} alt="profile" />
             </div>
             <div className="portfolio-about-me__title-text-wrapper">
-                <span className="portfolio-about-me__title">full stack web developer</span>
+                <span className="portfolio-about-me__subtitle">Hello, I'm <span className="portfolio-about-me__subtitle--highlight">Daniel</span>. I'm thrilled that you're here!</span>
                 <p className="portfolio-about-me__text">
-
+                    I love coding beautiful, simple things that get the job done as effectively as possible. I specialise in <span className="portfolio-about-me__text-highlight1">React & CSS/Sass for the front-end</span>, <span className="portfolio-about-me__text-highlight2">Express for the back-end</span>, and have experience managing <span className="portfolio-about-me__text-highlight3">databases with MongoDB</span>.
+                </p>
+                <p className="portfolio-about-me__text">
+                    Welcome to my portfolio!
                 </p>
             </div>
         </section>
