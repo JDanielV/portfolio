00d0c6b228201d91fbb2c071a1397e0d9b526c90
Daniel Villaverde <52183446+JDanielV@users.noreply.github.com>
2021-05-07 12:59:19 -0700

    made SWITCH statement more efficient, fixed HERO IMAGE responsiveness for mobile devices

--- client/src/Components/portfolio/ProjectsSection.js
+++ client/src/Components/portfolio/ProjectsSection.js
@@ -58,27 +58,6 @@ const ProjectsSection = ({ breakpoint }) => {
             </Swiper>)
             break;
         case 'tablet':
-            projectsComponent = (
-                <div className="portfolio-projects__project-container">
-                    <div className="portfolio-projects__project-img-side">
-                        <div className="portfolio-projects__project-img-wrapper">
-                            <img className="portfolio-projects__project-img" src="" alt="" />
-                        </div>
-                    </div>
-                    <div className="portfolio-projects__project-info-side">
-                        <h3 className="portfolio-projects__project-title"></h3>
-                        <p className="portfolio-projects__project-subtitle"></p>
-                        <div className="portfolio-projects__project-details-button-wrapper">
-                            <span className="portfolio-projects__project-details-button">more info</span>
-                            <div className="portfolio-projects__project-details-wrapper">
-                                <p className="portfolio-projects__project-details">
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            );
-            break;
         case 'desktop':
             projectsComponent = (
                 <div className="portfolio-projects__project-container">
