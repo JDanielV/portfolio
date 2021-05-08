e0f52b32dbbce91b8590545dc2f53365ecd9183e
Daniel Villaverde <52183446+JDanielV@users.noreply.github.com>
2021-05-07 21:11:42 -0700

    quick fix to torum FEATURES

--- client/src/Components/portfolio/ProjectsSection.js
+++ client/src/Components/portfolio/ProjectsSection.js
@@ -6,6 +6,7 @@ import "swiper/swiper-bundle.css";
 import TerranovaImage from "../../assets/portfolio-assets/terranova-mobile-cropped.jpg";
 import TorumImage from "../../assets/portfolio-assets/torum-mobile-cropped.jpg";
 import LightMindImage from "../../assets/portfolio-assets/lightmind-mobile.JPG";
+import { Link } from "react-router-dom";
 
 SwiperCore.use([Pagination]);
 
@@ -16,19 +17,22 @@ const ProjectsSection = ({ breakpoint }) => {
             projectName: "Terranova Sales",
             projectSubtitle: "Front-end, Maps API, back-end",
             projectDescription: "Business based in Downtown Vancouver (Canada). Developed & published their website.",
-            projectImage: TerranovaImage
+            projectImage: TerranovaImage,
+            projectLink: "https://terranovasales.ca/"
         },
         {
             projectName: "Light Mind",
             projectSubtitle: "Front-end, UX/UI design, back-end",
             projectDescription: "Entry-logger project built for BrainStation's Web Development Bootcamp program. Still under development.",
-            projectImage: LightMindImage
+            projectImage: LightMindImage,
+            projectLink: "https://www.loom.com/share/0346866e010d440a8dd79e73877ce734"
         },
         {
             projectName: "Torum Website (Practice)",
             projectSubtitle: "Front-end",
             projectDescription: "Developed for practice. I have no affiliation to Torum, nor I am endorsing their platform.",
-            projectImage: TorumImage
+            projectImage: TorumImage,
+            projectLink: "/torum"
         },
     ]
 
@@ -36,77 +40,171 @@ const ProjectsSection = ({ breakpoint }) => {
 
     switch (breakpoint) {
         case 'mobile':
-            projectsComponent = (<Swiper id="portfolio-projects__swiper-container" wrapperTag="ul" pagination spaceBetween={50}>
-                {projectSlides.map((project) => {
-                    return (
-                        <SwiperSlide tag="li" key={project.projectName}>
-                            <div className="portfolio-projects__project-container">
-                                <div className="portfolio-projects__project-img-side">
-                                    <div className="portfolio-projects__project-img-wrapper">
-                                        <img className="portfolio-projects__project-img" src={project.projectImage} alt="" />
-                                    </div>
-                                </div>
-                                <div className="portfolio-projects__project-info-side">
-                                    <h3 className="portfolio-projects__project-title">{project.projectName}</h3>
-                                    <span className="portfolio-projects__project-subtitle">{project.projectSubtitle}</span>
-                                    <div className="portfolio-projects__project-details-button-wrapper">
-                                        <div className="portfolio-projects__project-details-wrapper">
-                                            <p className="portfolio-projects__project-details">
-                                                {project.projectDescription}
-                                            </p>
+            projectsComponent =
+                (<Swiper id="portfolio-projects__swiper-container" wrapperTag="ul" pagination spaceBetween={50}>
+                    {projectSlides.map((project) => {
+                        const internalLink = project.projectLink === "/torum";
+                        return (
+                            <SwiperSlide tag="li" key={project.projectName}>
+                                {internalLink ?
+                                    <Link className="portfolio-projects__project-link" to={project.projectLink}>
+                                        <div className="portfolio-projects__project-container">
+                                            <div className="portfolio-projects__project-img-side">
+                                                <div className="portfolio-projects__project-img-wrapper">
+                                                    <img className="portfolio-projects__project-img" src={project.projectImage} alt="" />
+                                                </div>
+                                            </div>
+                                            <div className="portfolio-projects__project-info-side">
+                                                <h3 className="portfolio-projects__project-title">{project.projectName}</h3>
+                                                <span className="portfolio-projects__project-subtitle">{project.projectSubtitle}</span>
+                                                <div className="portfolio-projects__project-details-button-wrapper">
+                                                    <div className="portfolio-projects__project-details-wrapper">
+                                                        <p className="portfolio-projects__project-details">
+                                                            {project.projectDescription}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                    )
-                })}
-            </Swiper>)
+                                    </Link>
+                                    :
+                                    <a className="portfolio-projects__project-link" href={project.projectLink} target="_blank" rel="noreferrer">
+                                        <div className="portfolio-projects__project-container">
+                                            <div className="portfolio-projects__project-img-side">
+                                                <div className="portfolio-projects__project-img-wrapper">
+                                                    <img className="portfolio-projects__project-img" src={project.projectImage} alt="" />
+                                                </div>
+                                            </div>
+                                            <div className="portfolio-projects__project-info-side">
+                                                <h3 className="portfolio-projects__project-title">{project.projectName}</h3>
+                                                <span className="portfolio-projects__project-subtitle">{project.projectSubtitle}</span>
+                                                <div className="portfolio-projects__project-details-button-wrapper">
+                                                    <div className="portfolio-projects__project-details-wrapper">
+                                                        <p className="portfolio-projects__project-details">
+                                                            {project.projectDescription}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                }
+                            </SwiperSlide>
+                        )
+                    })}
+                </Swiper>)
             break;
         case 'tablet':
         case 'desktop':
             projectsComponent = (
-                <div className="portfolio-projects__project-container">
-                    <div className="portfolio-projects__project-img-side">
-                        <div className="portfolio-projects__project-img-wrapper">
-                            <img className="portfolio-projects__project-img" src="" alt="" />
-                        </div>
-                    </div>
-                    <div className="portfolio-projects__project-info-side">
-                        <h3 className="portfolio-projects__project-title"></h3>
-                        <p className="portfolio-projects__project-subtitle"></p>
-                        <div className="portfolio-projects__project-details-button-wrapper">
-                            <span className="portfolio-projects__project-details-button">more info</span>
-                            <div className="portfolio-projects__project-details-wrapper">
-                                <p className="portfolio-projects__project-details">
-                                </p>
+                <>
+                    {projectSlides.map((project) => {
+                        const internalLink = project.projectLink === "/torum";
+
+                        return (
+                            <div key={project.projectName}>
+                                {internalLink ?
+                                    <Link className="portfolio-projects__project-link" to={project.projectLink}>
+                                        <div className="portfolio-projects__project-container">
+                                            <div className="portfolio-projects__project-img-side">
+                                                <div className="portfolio-projects__project-img-wrapper">
+                                                    <img className="portfolio-projects__project-img" src={project.projectImage} alt="" />
+                                                </div>
+                                            </div>
+                                            <div className="portfolio-projects__project-info-side">
+                                                <h3 className="portfolio-projects__project-title">{project.projectName}</h3>
+                                                <span className="portfolio-projects__project-subtitle">{project.projectSubtitle}</span>
+                                                <div className="portfolio-projects__project-details-button-wrapper">
+                                                    <div className="portfolio-projects__project-details-wrapper">
+                                                        <p className="portfolio-projects__project-details">
+                                                            {project.projectDescription}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Link>
+                                    :
+                                    <a className="portfolio-projects__project-link" href={project.projectLink} target="_blank" rel="noreferrer">
+                                        <div className="portfolio-projects__project-container">
+                                            <div className="portfolio-projects__project-img-side">
+                                                <div className="portfolio-projects__project-img-wrapper">
+                                                    <img className="portfolio-projects__project-img" src={project.projectImage} alt="" />
+                                                </div>
+                                            </div>
+                                            <div className="portfolio-projects__project-info-side">
+                                                <h3 className="portfolio-projects__project-title">{project.projectName}</h3>
+                                                <span className="portfolio-projects__project-subtitle">{project.projectSubtitle}</span>
+                                                <div className="portfolio-projects__project-details-button-wrapper">
+                                                    <div className="portfolio-projects__project-details-wrapper">
+                                                        <p className="portfolio-projects__project-details">
+                                                            {project.projectDescription}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                }
                             </div>
-                        </div>
-                    </div>
-                </div>
-            );
+                        )
+                    })}
+                </>)
             break;
         default:
             projectsComponent = (
-                <div className="portfolio-projects__project-container">
-                    <div className="portfolio-projects__project-img-side">
-                        <div className="portfolio-projects__project-img-wrapper">
-                            <img className="portfolio-projects__project-img" src="" alt="" />
-                        </div>
-                    </div>
-                    <div className="portfolio-projects__project-info-side">
-                        <h3 className="portfolio-projects__project-title"></h3>
-                        <p className="portfolio-projects__project-subtitle"></p>
-                        <div className="portfolio-projects__project-details-button-wrapper">
-                            <span className="portfolio-projects__project-details-button">more info</span>
-                            <div className="portfolio-projects__project-details-wrapper">
-                                <p className="portfolio-projects__project-details">
-                                </p>
+                <>
+                    {projectSlides.map((project) => {
+                        const internalLink = project.projectLink === "/torum";
+                        return (
+                            <div key={project.projectName}>
+                                {internalLink ?
+                                    <Link className="portfolio-projects__project-link" to={project.projectLink}>
+                                        <div className="portfolio-projects__project-container">
+                                            <div className="portfolio-projects__project-img-side">
+                                                <div className="portfolio-projects__project-img-wrapper">
+                                                    <img className="portfolio-projects__project-img" src={project.projectImage} alt="" />
+                                                </div>
+                                            </div>
+                                            <div className="portfolio-projects__project-info-side">
+                                                <h3 className="portfolio-projects__project-title">{project.projectName}</h3>
+                                                <span className="portfolio-projects__project-subtitle">{project.projectSubtitle}</span>
+                                                <div className="portfolio-projects__project-details-button-wrapper">
+                                                    <div className="portfolio-projects__project-details-wrapper">
+                                                        <p className="portfolio-projects__project-details">
+                                                            {project.projectDescription}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Link>
+                                    :
+                                    <a className="portfolio-projects__project-link" href={project.projectLink} target="_blank" rel="noreferrer">
+                                        <div className="portfolio-projects__project-container">
+                                            <div className="portfolio-projects__project-img-side">
+                                                <div className="portfolio-projects__project-img-wrapper">
+                                                    <img className="portfolio-projects__project-img" src={project.projectImage} alt="" />
+                                                </div>
+                                            </div>
+                                            <div className="portfolio-projects__project-info-side">
+                                                <h3 className="portfolio-projects__project-title">{project.projectName}</h3>
+                                                <span className="portfolio-projects__project-subtitle">{project.projectSubtitle}</span>
+                                                <div className="portfolio-projects__project-details-button-wrapper">
+                                                    <div className="portfolio-projects__project-details-wrapper">
+                                                        <p className="portfolio-projects__project-details">
+                                                            {project.projectDescription}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                }
                             </div>
-                        </div>
-                    </div>
-                </div>
-            );
+                        )
+                    })}
+                </>)
     }
 
     return (
