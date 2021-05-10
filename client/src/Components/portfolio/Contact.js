e5bfe04933dab5f3d3e55d12ed2f0a0cdb17bbb6
Daniel Villaverde <52183446+JDanielV@users.noreply.github.com>
2021-05-10 00:21:54 -0700

    prepped contact form front end for back end integration

--- client/src/Components/portfolio/Contact.js
+++ client/src/Components/portfolio/Contact.js
@@ -1,16 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../../styles/main.css";
 import SendArrow from "../../assets/portfolio-assets/send-arrow.svg";
 import LinkedInIcon from "../../assets/portfolio-assets/linkedin.svg";
 
 
 const Contact = () => {
+
+    const [contactFormValues, setContactFormValues] = useState({
+        name: "",
+        email: "",
+        company: "",
+        message: ""
+    });
+
+    const [messageSent, setMessageSent] = useState(false);
+
+    const handleContactFormValues = (e) => {
+
+        switch (e.target.name) {
+            case 'name':
+                setContactFormValues({
+                    ...contactFormValues,
+                    name: e.target.value
+                });
+                break;
+            case 'email':
+                setContactFormValues({
+                    ...contactFormValues,
+                    email: e.target.value
+                });
+                break;
+            case 'company':
+                setContactFormValues({
+                    ...contactFormValues,
+                    company: e.target.value
+                });
+                break;
+            case 'message':
+                setContactFormValues({
+                    ...contactFormValues,
+                    message: e.target.value
+                });
+                break;
+        }
+    };
+
+    const resetForm = () => {
+        document.getElementById("contact-form").reset();
+    };
+
+    const submitContactForm = (e) => {
+        e.preventDefault();
+
+        if (!contactFormValues.name || !contactFormValues.email || !contactFormValues.message) {
+            //     setEmptyFields(true);
+            //   } else {
+            //     setEmptyFields(false);
+            //     axios
+            //       .post("/api", data)
+            //       .then((response) => {
+            //         console.log(response);
+            //         setMessageSent(true);
+            //         resetForm();
+            //       })
+            //       .catch((err) => {
+            //         console.log(err);
+            //         setMessageSent(false);
+            //       });
+            //   }
+            console.log("NOPE - FILL OUT ALL FIELDS");
+        }
+        else {
+            console.log("message sent :)")
+            resetForm();
+        }
+    }
+
     return (
         <section className="portfolio-contact" id="contact-section">
             <h2 className="portfolio-contact__title">Get In Touch</h2>
             <div className="portfolio-contact__main-wrapper">
                 <div className="portfolio-contact__left-side-wrapper">
-
                     <span className="portfolio-contact__contact-text--bold">Email me:</span>
                     <span className="portfolio-contact__contact-text--highlight">j.danielv@outlook.com</span>
                     <div className="portfolio-contact__divider" />
@@ -22,20 +92,20 @@ const Contact = () => {
                     </div>
                 </div>
                 <div className="portfolio-contact__right-side-wrapper">
-                    <form className="portfolio-contact__form-wrapper">
+                    <form className="portfolio-contact__form-wrapper" id="contact-form" onSubmit={submitContactForm}>
                         <div>
                             <span className="portfolio-contact__form-message-span">Contact Details</span>
                             <div className="portfolio-contact__form-inputs-wrapper">
-                                <input type="text" id="contact-name" className="portfolio-contact__form-input" placeholder="*Full Name" />
-                                <input type="text" id="contact-email" className="portfolio-contact__form-input" placeholder="*Email" />
-                                <input type="text" id="contact-company" className="portfolio-contact__form-input" placeholder="Company (optional)" />
+                                <input type="text" name="name" className="portfolio-contact__form-input" placeholder="*Full Name" onChange={handleContactFormValues} />
+                                <input type="text" name="email" className="portfolio-contact__form-input" placeholder="*Email" onChange={handleContactFormValues} />
+                                <input type="text" name="company" className="portfolio-contact__form-input" placeholder="Company (optional)" onChange={handleContactFormValues} />
                             </div>
                         </div>
                         <div>
                             <span className="portfolio-contact__form-message-span">Message</span>
-                            <textarea id="contact-message" className="portfolio-contact__form-textarea" placeholder="*How's your day going?" />
+                            <textarea type="text" name="message" className="portfolio-contact__form-textarea" placeholder="*How's your day going?" onChange={handleContactFormValues} />
                         </div>
-                        <button type="submit" className="portfolio-contact__form-button-wrapper">
+                        <button type="submit" className="portfolio-contact__form-button-wrapper" >
                             <span className="portfolio-contact__form-button-text">Send</span>
                             <img className="portfolio-contact__form-button-arrow" src={SendArrow} alt="send message" />
                         </button>
