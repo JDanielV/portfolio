8ba8dff5663cb27da57acb14e62f81c90da4746c
Daniel Villaverde <j.danielv@outlook.com>
2025-01-30 22:15:47 -0800

    did `npm i`

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import TorumContainer from './Components/torum/TorumContainer';
-import PortfolioContainer from './Components/portfolio/PortfolioContainer';
-import { Route, Switch, Redirect, BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import TorumContainer from "./Components/Torum/TorumContainer";
+import PortfolioContainer from "./Components/portfolio/PortfolioContainer";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-
   return (
     <div className="App">
       <Router>
-        {/* <Redirect exact from="/torum" to="/" /> */}
         <Switch>
           <Route path="/" exact>
             <PortfolioContainer />
